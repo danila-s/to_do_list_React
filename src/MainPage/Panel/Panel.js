e39c5c4a4494e27a02aebe98cc1bb6d461a944bc
Danila Shloma <ds.shloma@gmail.com>
2022-05-11 14:55:15 +0300

    onMouse reaction

--- src/MainPage/Panel/Panel.js
+++ src/MainPage/Panel/Panel.js
@@ -5,6 +5,10 @@ import { addNewTask } from '../../redux/actions'
 
 class Panel extends React.Component {
 
+    state = {
+        isActive: false
+    }
+
     addNewTask = (e) => {
         e.preventDefault();
         const form = new FormData(e.target);
@@ -12,8 +16,9 @@ class Panel extends React.Component {
     }
 
     render() {
+        const { isActive } = this.state;
         return (
-            <div className="panel">
+            (isActive && < div className="panel" onMouseLeave={() => { this.setState({ isActive: false }) }}>
                 <h1>Создать новую задачу</h1>
                 <form onSubmit={this.addNewTask}>
                     <label >Когда необходимо выполнить задачу ? :
@@ -24,7 +29,9 @@ class Panel extends React.Component {
                     <textarea maxLength='150' name='task-text'></textarea>
                     <input type='submit'></input>
                 </form>
-            </div>
+            </div > || <div className="create-new-task" onMouseEnter={() => { this.setState({ isActive: true }) }}>
+                    <div >Добавить задачу</div>
+                </div>)
         )
     }
 }
